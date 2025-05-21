229669d4d747018252272cb5ec2f9b16dbb33bf1
yelamoste <108941678+yelamoste@users.noreply.github.com>
2025-05-21 23:24:32 +0800

    update Home.jsx

--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex flex-col bg-amber-50 w-[60%] h-[70%] self-center text-wrap">
-      <p>Player, Welcome to the Faraway Land</p>
+    <div className="flex flex-col bg-amber-50 border-2 w-[60%] h-[70%] max-md:w-[90%] self-center text-wrap">
+      <div className="h-[80%] w-[100%]"></div>
+      <div className="h-[20%] w-[90%] justify-center self-center mb-[10px]  rounded-[6px] bg-amber-950">
+        <p className="font-silkscreen text-white m-[15px]">
+          Player, Welcome to the Faraway Land
+        </p>
+        <button className="z-1 w-[10%] h-[50%] border-2 rounded-[10px] cursor-pointer bg-gray-950 text-white">
+          Next
+        </button>
+      </div>
     </div>
   );
 };
